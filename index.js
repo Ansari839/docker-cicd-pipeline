f00c59d5cb5b16153a564dea802bcf87bb39bcd9
Abdullah Ansari <137299911+Ansari839@users.noreply.github.com>
2025-01-03 00:34:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! and build Docker');
+    res.send('Hello, World! and build Docker check');
 });
 
 const PORT = process.env.PORT || 3000;
